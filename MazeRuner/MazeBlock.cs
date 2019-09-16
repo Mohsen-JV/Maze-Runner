770e3a3c54b93b208f02a9617400afe2cddb989a
M_JV <mojashan@gmail.com>
2019-09-16 11:36:26 +0430

    Update

--- MazeRuner/MazeBlock.cs
+++ MazeRuner/MazeBlock.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Media;
-using System.Threading.Tasks;
-
-namespace MazeRuner
+﻿namespace MazeRuner
 {
     public enum Direcion { Null, N, NE, E, SE, S, SW, W, NW }
     public class Block
@@ -13,7 +7,7 @@ namespace MazeRuner
         public bool Seen = false;
         public bool Checked = false;
         bool Is8Dir = false;
-        Direcion _Dir=Direcion.Null;
+        Direcion _Dir = Direcion.Null;
         Direcion NextDirection(Direcion dir)
         {
             if (Is8Dir)
@@ -24,12 +18,12 @@ namespace MazeRuner
         public Block(bool is8dir) => Is8Dir = is8dir;
         public void NextDir()
         {
-            if(_Dir!=Direcion.Null)
+            if (_Dir != Direcion.Null)
             {
                 _Dir = NextDirection(_Dir);
-                if(_Dir==Direcion.E)
+                if (_Dir == Direcion.E)
                 {
-                    Checked= true;
+                    Checked = true;
                     _Dir = Direcion.Null;
                 }
             }
@@ -62,7 +56,7 @@ namespace MazeRuner
     {
         Block[,] Mat;
         public bool Is8Dir;
-        public Maze(int row,int column,bool is8dir=false)
+        public Maze(int row, int column, bool is8dir = false)
         {
             Is8Dir = is8dir;
             Mat = new Block[row + 2, column + 2];
@@ -75,9 +69,12 @@ namespace MazeRuner
                 for (int ip = 1; ip <= column; ip++)
                     Mat[i, ip] = new Block(is8dir);
         }
-        public Block this[int row,int column]
+        public Block this[int row, int column]
         {
             get => Mat[row + 1, column + 1];
+            set => Mat[row + 1, column + 1] = value;
         }
+        public int Row => Mat.GetLength(0) - 2;
+        public int Column => Mat.GetLength(1) - 2;
     }
 }
